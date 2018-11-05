b094517c0c4e2e01b3efcaaf33ebcab1191eacce
Sincoyw <heaven_20062006@126.com>
2018-11-05 19:29:34 +0800

    Add custom error page for 404 error.

---
+++ src/main/java/com/sincoyw/breakserver/exception/MyErrorController.java
@@ -0,0 +1,19 @@
+package com.sincoyw.breakserver.exception;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error_404";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
